959f767f1c45a00a677c6de68dfa6ed7502804b1
Mark Fisher and Oliver Fu <mark@>
2017-03-06 14:44:06 -0800

    add Reply, Claim, and Comment object constructors, as well as beggining of a test function

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,47 @@
 // Back End
 
+function Reply(replier, userReply){
+  this.replier = replier;
+  this.userReply= userReply;
+}
+
+function Claim (claimer, userClaim){
+  this. claimer = claimer;
+  this. userClaim = userClaim;
+  this.upVoteArray = [];
+  this.downVoteArray = [];
+  this.upCount = this.upVoteArray.length;
+  this.downCount = this.downVoteArray.length;
+  this.pro = new Support();
+  this.con = new Support();
+  this.claimComments = [];
+}
+
+function Comment(commenter, userComment){
+  this.commenter = commenter;
+  this.userComment = userComment;
+  this.replies = [];
+}
+
+function test(){
+  var userID = "Jahan";
+  var Claim1 = new Claim(userID, "The ninth floor bathroom is better than the eighth floor bathroom");
+  console.log(Claim1);
+  Claim1.pro.source.push(new Source("http://www.google.com",userID))
+  var user2 = "Oliver";
+  var obnoxiousComment = new Comment (user2, "You suck, Jahan!");
+  var user3 = "Chance";
+  var irrelevantReply = new Reply (user3, "check out my new rap album!");
+  obnoxiousComment.replies.push(irrelevantReply);
+
+
+}
+
 // Front End
 $(function(){
 
 });
+
+
+
+Claim1.pro.push(source1)
