9dba15a57da661d939d8f929d9c48dbd677c148a
chance magno and jahan walsh <jahan@>
2017-03-06 14:42:25 -0800

    add Object for support and source with arrays.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 // Back End
+function Support(){
+  this.proSupport = [];
+  this.conSupport = [];
+  this.source = [];
+}
 
+function Source(){
+  this.upVote = [];
+  this.downVote = [];
+  this.link = [];
+}
 // Front End
 $(function(){
 
