764c2f0f6771634b93648f551378621430ac7b15
Mark Fisher and Oliver Fu <oliver@>
2017-03-06 16:37:15 -0800

    add front end click event listener for newClaimSubmit div

--- js/scripts.js
+++ js/scripts.js
@@ -45,6 +45,8 @@ function test(){
   console.log(Claim1.upCount);
   console.log(Claim1.downCount);
   console.log(Claim1);
+  //test newClaim
+  var newClaim = new NewClaim("Jahan", "Tap water is safer than bottled water");
 }
 
 function Source(link, sourcer){
@@ -54,9 +56,38 @@ function Source(link, sourcer){
   this.sourcer = sourcer;
 }
 
+//newClaim. must know userID, store a string as a new claim. Dynamically interact with HTML.
+//frontEnd: upon click function, store userID.
+// function  NewClaim(userID, newclaim){
+//   this.userID = userID;
+//   this.newClaim = newClaim;
+
+}
+
 // Front End
 $(function(){
+  $("#newClaimButton").click(function(){
+    //instead prompt, place ansewrs in initially hidden form
+    $("#newClaimForm").show();
+    var newClaimSender = $("input#newClaimSender").val();
+    var newClaim = $("input#newClaim").val();
 
+
+    // var newClaimer = prompt("What is your username?")
+    // var newClaim =  prompt("Please enter a claim")
+    // if (newClaim != null && newClaimer!=null)
+    //   $("#newClaimArea").append("<h4>" + newClaim + "</h4>");
+      var createClaim = new Claim (newClaimer, newClaim);
+    }
+
+  $("#newClaimSubmit").click(function(){
+    var newClaimSender = $("input#newClaimSender").val();
+    var newClaim = $("input#newClaim").val();
+    var createClaim = new Claim (newClaimer, newClaim);
+  })
+
+
+  })
 });
 
 
