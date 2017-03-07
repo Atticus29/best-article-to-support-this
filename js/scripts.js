1c9c24292943f945bb200381301ef80059de3b34
chance magno and jahan walsh <jahan@>
2017-03-07 13:45:21 -0800

    add comment box, front end started, user must be signed in to comment, signed in user comment will append to #commentSection

--- js/scripts.js
+++ js/scripts.js
@@ -182,9 +182,12 @@ $("#signout-btn").click(function(){
   //claimComments
   $("#commentForm").submit(function(){
     event.preventDefault();
-    console.log($("#commentSection").append("<p>" + userName + "commented" +($("#comments").val())));
+      if (userName.length > 1) {
+        $("#commentSection").append("<p>" + userName + " commented" + $("#comments").val() + "</p>");
+      } else {
+        alert("You must be logged in to comment")
+      }
   });
-
 });
 
 
